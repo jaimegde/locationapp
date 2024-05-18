89c8de34a7522da5b08616f7afd0813825259603
Jaime Garcia de Enterria <jaimenterria@gmail.com>
2024-05-18 13:41:50 +0200

    working on location composable

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -1,13 +1,22 @@
 package com.example.locationapp
 
+import android.content.Context
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.locationapp.ui.theme.LocationAppTheme
@@ -22,7 +31,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
@@ -30,17 +39,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun LocationDisplay(locationUtils: LocationUtils, context: Context) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LocationAppTheme {
-        Greeting("Android")
-    }
-}
+    val requestPermissionLauncher =
+        rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestMultiplePermissions(),
+            onResult = { permissions ->
+                if (permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                    &&
+                    permissions[Manifest.permission.ACCESS_COARSE_LOCATION] == true
+                ) {
+                    //Location successfull
+
+                } else {
+
+                }
+            })
+
+            Column(
+                modifier = Modifier.fillMaxSize(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+                verticalArrangement = Arrangement.Center
+            ) {
+                Text("Location not available")
+
+                Button(onClick = {
+                    if (locationUtils.hasLocationPermission(context)) {
+                        //Permission already granted, update location
+
+                    } else {
+                        //Request location permission
+                    }
+                }) {
+                    Text("Get Location")
+                }
+            }
+        }
